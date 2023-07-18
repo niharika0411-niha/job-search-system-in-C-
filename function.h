e0687f9a9f5d016e57bc5e3031f3ca63b7279059
niharika0411-niha <135304573+niharika0411-niha@users.noreply.github.com>
2023-07-19 03:46:28 +0530

    Create function.h

---
+++ function.h
@@ -0,0 +1,9 @@
+struct node *create_node(void);
+void transverse(struct node *start);
+void add_beg(struct node *start,int val);
+void add_at_end(struct node *start,int val);
+void del_beg(struct node *start);
+void insert_at_mid(struct node *start,int val);
+void del_mid(struct node *start);
+void print_beg(struct node *start,int i);
+void size(struct node* start);
